deaa45d1b2d6bf094fc4973e08c50c18abf9d8ef
unknown <onghoangcodebug01@gmail.com>
2022-10-24 16:49:33 +0700

    1:42:03

--- counter.js
+++ counter.js
@@ -1,12 +1,18 @@
-let cnt=0;
+if (!localStorage.getItem('cnt'))
+    localStorage.setItem('cnt',0);
 function add(){
+    let cnt =localStorage.getItem('cnt');
     cnt++;
     document.querySelector('h1').innerHTML= cnt;
-    if (cnt % 5 === 0){
+    if (cnt % 500 === 0){
         alert(`Counter is now ${cnt}`);
     }
+    localStorage.setItem('cnt',cnt);
 
 }
 document.addEventListener("DOMContentLoaded",function(){
-    document.querySelector('button').onclick = add;
+    document.querySelector('h1').innerHTML=localStorage.getItem('cnt');
+    document.querySelector("button").onclick = add;
+    
+    // setInterval(add,1000);
 });
